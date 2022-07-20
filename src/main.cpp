b506e33d03803d0a7d51c8af337296f701d459a4
Cattimus <devcattimus@gmail.com>
2022-07-19 17:39:54 -0700

    Updated main to minimal working glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
-using namespace std;
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
+//imgui includes
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+const int width = 640;
+const int height = 480;
+
+//set up values for camera
+glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
 
 int main()
 {
-	cout << "hello world" << endl;
-	return 0;
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	auto window = glfwCreateWindow(width, height, "Untitled Meat Game", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	//load extensions
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to init GLAD" << std::endl;
+		return -1;
+	}
+
+	//enable vsync
+	glfwSwapInterval(1);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 }
